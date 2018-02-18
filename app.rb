2daebbb2c6a8428b1b4f819c08c422a676c6e20a
Miwa <paintfx@gmail.com>
2018-02-18 16:34:07 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is mietone"
+  end
+
+  get '/hometown' do
+    "My hometown is Tokyo"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is near light"
+  end
 end
