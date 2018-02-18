d13d0afc3d5dcc443339e833a02add40ba43b201
Miwa <paintfx@gmail.com>
2018-02-18 16:06:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is mietone"
+  end
+
+  get '/hometown' do
+    "My hometown is Tokyo"
+  end
+
+  get '/favorite-song' do
+    "My favorte song is raein"
+  end
 end
